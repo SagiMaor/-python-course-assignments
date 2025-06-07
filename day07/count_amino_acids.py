ad16ea9996e60b10a2ad3428ed9bdc895326bfee
SagiMaor <95123398+SagiMaor@users.noreply.github.com>
2025-06-07 10:26:05 +0300

    Create count_amino_acids.py

---
+++ day07/count_amino_acids.py
@@ -0,0 +1,45 @@
+codon_table = {
+    'Phe': ['TTT', 'TTC'],
+    'Leu': ['TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'],
+    'Ile': ['ATT', 'ATC', 'ATA'],
+    'Met': ['ATG'],
+    'Val': ['GTT', 'GTC', 'GTA', 'GTG'],
+    'Ser': ['TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'],
+    'Pro': ['CCT', 'CCC', 'CCA', 'CCG'],
+    'Thr': ['ACT', 'ACC', 'ACA', 'ACG'],
+    'Ala': ['GCT', 'GCC', 'GCA', 'GCG'],
+    'Tyr': ['TAT', 'TAC'],
+    'His': ['CAT', 'CAC'],
+    'Gln': ['CAA', 'CAG'],
+    'Asn': ['AAT', 'AAC'],
+    'Lys': ['AAA', 'AAG'],
+    'Asp': ['GAT', 'GAC'],
+    'Glu': ['GAA', 'GAG'],
+    'Cys': ['TGT', 'TGC'],
+    'Trp': ['TGG'],
+    'Arg': ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
+    'Gly': ['GGT', 'GGC', 'GGA', 'GGG'],
+    'STOP': ['TAA', 'TAG', 'TGA']
+}
+
+# Convert to codon -> amino acid mapping
+codon_to_amino = {}
+for amino, codons in codon_table.items():
+    for codon in codons:
+        codon_to_amino[codon] = amino
+
+# Read DNA sequence from file
+with open("sequence.txt") as f:
+    sequence = f.read().strip().upper()
+
+# Count amino acids
+amino_counts = {}
+for i in range(0, len(sequence) - 2, 3):
+    codon = sequence[i:i+3]
+    if codon in codon_to_amino:
+        amino = codon_to_amino[codon]
+        amino_counts[amino] = amino_counts.get(amino, 0) + 1
+
+# Print result
+for amino in sorted(amino_counts):
+    print(f"{amino}: {amino_counts[amino]}")
