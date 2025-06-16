b5e6481772e2c8613c5790e54b0ccaed28245499
SagiMaor <95123398+SagiMaor@users.noreply.github.com>
2025-06-16 18:26:31 +0300

    Update count_words_from_a_file.py

--- day07/count_words_from_a_file.py
+++ day07/count_words_from_a_file.py
@@ -1,5 +1,17 @@
+input_text = """Lorem ipsum dolor qui ad labor ad labor sint dolor  tempor incididunt ut labor ad dolore lorem ad
+Ut labor ad dolor lorem qui ad ut labor   ut ad commodo commodo
+Lorem ad dolor in reprehenderit in lorem ut labor ad dolore eu in labor dolor
+sint occaecat ad labor proident sint in in qui labor ad dolor ad in ad labor
+"""
+
 file_in = 'examples/dictionary/words_and_spaces.txt'
 file_out = 'examples/dictionary/words_and_spaces_counted.txt'
+
+import os
+os.makedirs(os.path.dirname(file_in), exist_ok=True)
+
+with open(file_in, 'w') as f:
+    f.write(input_text)
 
 with open(file_in, 'r') as f:
     text = f.read()
@@ -8,10 +20,7 @@ words = text.lower().split()
 counts = {}
 
 for word in words:
-    if word in counts:
-        counts[word] += 1
-    else:
-        counts[word] = 1
+    counts[word] = counts.get(word, 0) + 1
 
 with open(file_out, 'w') as f:
     for word in sorted(counts):
